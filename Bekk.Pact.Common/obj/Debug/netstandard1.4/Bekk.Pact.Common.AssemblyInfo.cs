682cd059841af6c581901eacfc8dbd2034a6a40b
Øyvind Skaar <oyvind.skaar@bekk.no>
2017-06-01 12:48:41 +0200

    Change some dependencies. Clean up csproj

--- Bekk.Pact.Common/obj/Debug/netstandard1.4/Bekk.Pact.Common.AssemblyInfo.cs
+++ Bekk.Pact.Common/obj/Debug/netstandard1.4/Bekk.Pact.Common.AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bekk.Pact.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("A library for implementing Pact provider tests")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bekk.Pact.Common")]
