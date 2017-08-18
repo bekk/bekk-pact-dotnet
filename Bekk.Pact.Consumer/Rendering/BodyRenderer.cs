e2aecafe0d11349bb5cd4a80449248359f770489
Øyvind Skaar <oyvind.skaar@bekk.no>
2017-08-18 18:24:14 +0200

    Rendering arrays

--- Bekk.Pact.Consumer/Rendering/BodyRenderer.cs
+++ Bekk.Pact.Consumer/Rendering/BodyRenderer.cs
@@ -17,26 +17,34 @@ namespace Bekk.Pact.Consumer.Rendering
             this.pact = pact;
         }
 
-        public JObject Render()
+        public JContainer Render()
         {
             body = pact?.ResponseBody;
             if (body == null) return null;
             var contentType = HeaderExtensions.ContentType(pact.ResponseHeaders);
             if (contentType != null && contentType != "application/json; charset=utf-8") throw new NotSupportedException(
                 $"Only content-type: application/json; charset=utf-8 is supported. Found: {contentType}");
+            return Render(body);
+        }
+
+        private JContainer Render(object body)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
             switch (body)
             {
                 case string serialized:
                     return JObject.Parse(serialized);
+                case Array array:
+                    return JArray.FromObject(body, JsonSerializer.Create(settings));
                 default:
-                    var settings = new JsonSerializerSettings()
-                    {
-                        Formatting = Formatting.Indented,
-                        ContractResolver = new CamelCasePropertyNamesContractResolver()
-                    };
                     return JObject.FromObject(body, JsonSerializer.Create(settings));
             }
         }
+
         
         public override string ToString() => Render().ToString();
     }
