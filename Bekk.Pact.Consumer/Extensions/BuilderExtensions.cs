918dda9768bc90f3d6184463d454bd280fa7bb7d
Øyvind Skaar <oyvind.skaar@bekk.no>
2017-08-18 19:27:59 +0200

    Add another extension method

--- Bekk.Pact.Consumer/Extensions/BuilderExtensions.cs
+++ Bekk.Pact.Consumer/Extensions/BuilderExtensions.cs
@@ -15,5 +15,9 @@ namespace Bekk.Pact.Consumer.Extensions
         /// Provide a header that is required in the request.
         /// </summary>
         public static IRequestBuilder WithHeader(this IRequestBuilder builder, string key, int value) => builder.WithHeader(key, value.ToString());
+        /// <summary>
+        /// Provide a query to add to the url.
+        /// </summary>
+        public static IRequestBuilder WithQuery(this IRequestBuilder builder, string key, int value) => builder.WithQuery(key, value.ToString());
     }
 }
