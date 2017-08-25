78b8c8ec60ad2c2e6e10ef44c7a665dab62b2591
Øyvind Skaar <oyvind.skaar@bekk.no>
2017-08-25 15:40:13 +0200

    Even more stupid bug

--- Bekk.Pact.Common/Extensions/ConfigurationExtensions.cs
+++ Bekk.Pact.Common/Extensions/ConfigurationExtensions.cs
@@ -9,19 +9,19 @@ namespace Bekk.Pact.Common.Extensions
         {
             if(config == null || level > config.LogLevel || text == null) return;
             config.Log(text);
-            LogToFile(config.LogFile, text);
+            LogToFile(config.LogFile, level, text);
         }
         public static void LogSafe(this Contracts.IConfiguration config, LogLevel level, Func<string> logMsg)
         {
             if(config == null || level > config.LogLevel || logMsg == null) return;
             var text = logMsg.Invoke();
             config.Log(text);
-            LogToFile(config.LogFile, text);
+            LogToFile(config.LogFile, level, text);
         }
-        private static void LogToFile(string path, string text)
+        private static void LogToFile(string path, LogLevel level, string text)
         {
             if(string.IsNullOrWhiteSpace(path)) return;
-            var txt = $"{DateTime.Now:o}: text{Environment.NewLine}";
+            var txt = $"{DateTime.Now:o} {level}: {text}{Environment.NewLine}";
             System.IO.File.AppendAllText(path, txt);
         }
     }
